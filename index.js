a6e6e7960a277f70dffd733f2d284ffbc4a3fcf3
cumthyj <cumthyj@outlook.com>
2018-08-20 23:31:13 +0800

    move list

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
+import MoveList from './pages/MoveList';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MoveList);
